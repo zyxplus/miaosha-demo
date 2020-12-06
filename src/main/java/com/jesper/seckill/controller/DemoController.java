c1c49fd5dccb854eb1300e7f4b3edad5e2d63a9d
zyxplus <714515681@qq.com>
2020-12-06 23:27:40 +0800

    Initial commit

---
+++ src/main/java/com/jesper/seckill/controller/DemoController.java
@@ -0,0 +1,30 @@
+package com.jesper.seckill.controller;
+
+import com.jesper.seckill.result.CodeMsg;
+import com.jesper.seckill.result.Result;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("demo")
+public class DemoController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public Result<String> hello() {
+        return Result.success("hello, Jesper");
+    }
+
+    @RequestMapping("/helloError")
+    @ResponseBody
+    public Result<String> error() {
+        return Result.error(CodeMsg.SERVER_ERROR);
+    }
+}
